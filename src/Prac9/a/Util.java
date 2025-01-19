34f2460eda06fcec3e172b9f64e8ce4375cb32f3
Tami Elb <Tami.dev@outlook.com>
2025-01-19 19:15:49 +0100

    commit

--- src/Prac9/a/Util.java
+++ src/Prac9/a/Util.java
@@ -2,14 +2,10 @@ package Prac9.a;
 
 public class Util {
     public static String euroBedrag(double bedrag) {
-        return formatString(bedrag, 2);
+        return euroBedrag(bedrag, 2);
     }
 
     public static String euroBedrag(double bedrag, int precisie) {
-        return formatString(bedrag, precisie);
-    }
-
-    private static  String formatString(double bedrag, int precisie) {
         String format = "%." + precisie + "f";
         return String.format(format, bedrag).replace('.', ',');
     }
